7be9491c2c297f1632bfea3af9e2ab87908b57a8
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-11-02 13:51:11 +0200

    Update Payment.java

--- Payment/src/java/Payment.java
+++ Payment/src/java/Payment.java
@@ -1,1 +1,70 @@
+import jakarta.persistence.*;
 
+@Entity
+@Table
+public class Payment {
+    @Id
+    @SequenceGenerator(
+            name = "payment_sequence",
+            sequenceName = "payment_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "payment_sequence"
+    )
+    private Long id;
+    private Long orderId;
+    private String paymentMethod;
+    private int paymentAmount;
+
+    public Payment() {
+    }
+
+    public Payment(String paymentMethod, int paymentAmount) {
+        this.paymentMethod = paymentMethod;
+        this.paymentAmount = paymentAmount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public int getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(int paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "id=" + id +
+                ", orderId=" + orderId +
+                ", paymentMethod='" + paymentMethod + '\'' +
+                ", paymentAmount=" + paymentAmount +
+                '}';
+    }
+}
