16c06da17e25d8d6b1d40f4bfe038be5d22dcca6
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-11-16 12:12:57 +0200

    Update OrdersController.java

--- Orders/src/java/OrdersController.java
+++ Orders/src/java/OrdersController.java
@@ -21,36 +21,52 @@ public class OrdersController {
 
     @GetMapping
     @Transactional(timeout = 500)
-    @CircuitBreaker(name = "circuitBreaker1", fallbackMethod = "fallback")
+    @CircuitBreaker(name = "circuitBreaker1", fallbackMethod = "fallback1")
     public List<Orders> getOrders() {
         return ordersService.getOrders();
     }
 
     @PostMapping()
     @Transactional(timeout = 500)
-    @CircuitBreaker(name = "circuitBreaker2", fallbackMethod = "fallback")
+    @CircuitBreaker(name = "circuitBreaker2", fallbackMethod = "fallback2")
     public void addOrder(@RequestBody Orders order) {
         ordersService.addOrder(order);
     }
 
     @DeleteMapping("{orderId}")
     @Transactional(timeout = 500)
-    @CircuitBreaker(name = "circuitBreaker3", fallbackMethod = "fallback")
+    @CircuitBreaker(name = "circuitBreaker3", fallbackMethod = "fallback3")
     public void deleteOrder(@PathVariable("orderId") Long orderId) {
         ordersService.deleteOrder(orderId);
     }
 
     @PatchMapping("{orderId}")
     @Transactional(timeout = 500)
-    @CircuitBreaker(name = "circuitBreaker4", fallbackMethod = "fallback")
+    @CircuitBreaker(name = "circuitBreaker4", fallbackMethod = "fallback4")
     public void changeAddress(
             @PathVariable("orderId") Long orderId,
             @RequestParam(required = false) String address) {
         ordersService.changeAddress(orderId, address);
     }
 
-    private String fallback(Throwable e) {
+    private String fallback1(Throwable e) {
         System.out.println("Exception happened : " + e.getMessage());
-        return "Handled the exception through fallback method";
+        return "Handled the exception through fallback method.";
+    }
+
+    private String fallback2(@RequestBody Orders order, Throwable e) {
+        System.out.println("Exception happened : " + e.getMessage());
+        return "Handled the exception through fallback method.";
+    }
+
+    private String fallback3(@PathVariable("orderId") Long orderId, Throwable e) {
+        System.out.println("Exception happened : " + e.getMessage());
+        return "Handled the exception through fallback method.";
+    }
+
+    private String fallback4(@PathVariable("orderId") Long orderId,
+                             @RequestParam(required = false) String address, Throwable e) {
+        System.out.println("Exception happened : " + e.getMessage());
+        return "Handled the exception through fallback method.";
     }
 }
