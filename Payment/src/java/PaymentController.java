f9bccc102366677b90e14d99b122d1c5f2db7fdb
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-11-16 12:14:04 +0200

    Update PaymentController.java

--- Payment/src/java/PaymentController.java
+++ Payment/src/java/PaymentController.java
@@ -21,37 +21,52 @@ public class PaymentController {
 
     @GetMapping
     @Transactional(timeout = 500)
-    @CircuitBreaker(name = "circuitBreaker1", fallbackMethod = "fallback")
+    @CircuitBreaker(name = "circuitBreaker1", fallbackMethod = "fallback1")
     public List<Payment> getPayments() {
         return paymentService.getPayments();
     }
 
     @PostMapping()
     @Transactional(timeout = 500)
-    @CircuitBreaker(name = "circuitBreaker2", fallbackMethod = "fallback")
+    @CircuitBreaker(name = "circuitBreaker2", fallbackMethod = "fallback2")
     public void addPayment(@RequestBody Payment payment) {
         paymentService.addPayment(payment);
     }
 
     @DeleteMapping("{paymentId}")
     @Transactional(timeout = 500)
-    @CircuitBreaker(name = "circuitBreaker3", fallbackMethod = "fallback")
+    @CircuitBreaker(name = "circuitBreaker3", fallbackMethod = "fallback3")
     public void deletePayment(@PathVariable("paymentId") Long paymentId) {
         paymentService.deletePayment(paymentId);
     }
 
     @PatchMapping("{paymentId}")
     @Transactional(timeout = 500)
-    @CircuitBreaker(name = "circuitBreaker4", fallbackMethod = "fallback")
+    @CircuitBreaker(name = "circuitBreaker4", fallbackMethod = "fallback4")
     public void changePaymentMethod(
             @PathVariable("paymentId") Long paymentId,
             @RequestParam(required = false) String paymentMethod) {
         paymentService.changePaymentMethod(paymentId, paymentMethod);
     }
 
-    private String fallback(Throwable e) {
-
+    private String fallback1(Throwable e) {
         System.out.println("Exception happened : " + e.getMessage());
-        return "Handled the exception through fallback method";
+        return "Handled the exception through fallback method.";
+    }
+
+    private String fallback2(@RequestBody Payment payment, Throwable e) {
+        System.out.println("Exception happened : " + e.getMessage());
+        return "Handled the exception through fallback method.";
+    }
+
+    private String fallback3(@PathVariable("paymentId") Long paymentId, Throwable e) {
+        System.out.println("Exception happened : " + e.getMessage());
+        return "Handled the exception through fallback method.";
+    }
+
+    private String fallback4(@PathVariable("paymentId") Long paymentId,
+                             @RequestParam(required = false) String paymentMethod, Throwable e) {
+        System.out.println("Exception happened : " + e.getMessage());
+        return "Handled the exception through fallback method.";
     }
 }
